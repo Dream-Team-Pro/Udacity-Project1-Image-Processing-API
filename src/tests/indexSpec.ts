cc34708b09775105d8ec6bab5f98cdec7612d11b
Dream-Team-Pro <mz1320021@yahoo.com>
2022-06-22 12:31:59 +0300

    complete the project

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,15 +1,22 @@
-import supertest from "supertest";
+import superTest from "superTest";
 import app from "../index";
 import fs from "fs";
 import path from "path";
-const request = supertest(app);
-import {createResizedImageDir} from "../utilities";
+const request = superTest(app);
+import {
+  resizeImage, 
+  createResizedImageDir,
+  imageValid
+} from "../utilities";
+
+import { Request, Response } from "express";
+import sharp from "sharp";
 
 const imagesPath = path.join(__dirname, "../..", "/public/images/");
 const FullPath = path.join(imagesPath, `/full/`);
 const ResizedPath = path.resolve(imagesPath, `resized/`);
 
-// jasmine unit testing
+// jasmine unit Testing
 // Test a simple api endpoints
 describe("Test endpoint response", () => {
   it("Gets the [/] endpoint", async () => {
@@ -43,7 +50,7 @@ describe("Test endpoint response", () => {
   });
 });
 
-describe("test check Url parameters", () => {
+describe("Test check Url parameters", () => {
   it("should True because all url parameters entered", async () => {
     await request.get("/api/images/?filename=fish&width=100&height=350");
     expect(fs.existsSync(`${ResizedPath}/fish-100-350.jpg`)).toBeDefined();
@@ -65,8 +72,8 @@ describe("test check Url parameters", () => {
   });
 });
 
-/*----- check to create Resized Directory if not Exists -----*/
-describe("test check create resized Directory", () => {
+/*----- Test the create Resized Directory function -----*/
+describe("Test the create Resized Directory function endpoint", () => {
   it("should True because it's create resized Directory", () => {
     createResizedImageDir().then(() => {
       const check = imagesPath + "/resized/";
@@ -79,5 +86,33 @@ describe("test check create resized Directory", () => {
       const check = imagesPath + "/newDir/";
       expect(fs.existsSync(check)).toBeFalse();
     });
+  });
+});
+
+/*---- Test the resize Image function -----*/
+describe("Test the resize Image function endpoint", () => {
+  it("should True because the filename found in FULL Directory", async () => {
+    resizeImage('whale', 100, 350).then( () => {
+        expect(fs.existsSync(`${ResizedPath}/whale-100-350.jpg`)).toBeDefined();     
+    })
+  });
+
+  it("should False because the filename found in FULL Directory", async () => {
+    resizeImage('dog', 100, 350).then( () => {
+        expect(fs.existsSync(`${ResizedPath}/dog-100-350.jpg`)).toBeFalse();
+    })
+  });
+});
+
+/*---- Test the image Valid function -----*/
+describe("Test the image Valid function endpoint", () => {
+  it("should True because the params is valid URL path", async () => {
+    imageValid('whale', 100, 350);
+    expect(imageValid).toBeTrue;    
+  });
+
+  it("should False because the params is not valid URL path", async () => {
+    imageValid('whale', 0, 0);
+    expect(imageValid).toBeFalse;
   });
 });
